75a4d9cedeacfa1fb03baa0d4ebd18746bde58d2
chrishayuk <chrishayuk@googlemail.com>
2025-03-29 23:38:01 +0000

    removed chuk-mcp and moved to it's own repo

--- src/chuk_mcp/src/mcp_client/messages/initialize/mcp_client_capabilities.py
+++
@@ -1,7 +0,0 @@
-# chuk_mcp/mcp_client/messages/initialize/mcp_client_capabilties.py
-from mcp_client.mcp_pydantic_base import McpPydanticBase, Field
-
-class MCPClientCapabilities(McpPydanticBase):
-    roots: dict = Field(default_factory=lambda: {"listChanged": True})
-    sampling: dict = Field(default_factory=dict)
-    experimental: dict = Field(default_factory=dict)
--- src/chuk_mcp/src/mcp_client/messages/tools/tool_result.py
+++
@@ -1,8 +0,0 @@
-# chuk_mcp/mcp_client/messages/tools/tool_input_schema.py
-from typing import Any, Dict, List
-from mcp_client.mcp_pydantic_base import McpPydanticBase, Field
-
-class ToolResult(McpPydanticBase):
-    """Model representing the result of a tool invocation."""
-    content: List[Dict[str, Any]]
-    isError: bool = False
